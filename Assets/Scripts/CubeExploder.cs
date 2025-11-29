cfd5fb493b371775421cc71415c237a4550e436b
Tensair <tensairusx@gmail.com>
2025-11-29 21:32:14 +0300

    Added explosion visual effect and PushCubesInRadius function on cube destruction without split

--- Assets/Scripts/CubeExploder.cs
+++ Assets/Scripts/CubeExploder.cs
@@ -1,8 +1,16 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class CubeExploder : MonoBehaviour
 {
+    [Header("Explosion Effect Parameters")]
+    [SerializeField] private GameObject _explosionEffect;
+    [SerializeField] private float _explosionEffectScale = 1.5f;
+    [SerializeField] private float _explosionEffectLifeTime = 1f;
+    [Header("Explosion Parameters")]
+    [SerializeField] private float _explosionDefaultRadius = 5f;
+    [SerializeField] private float _explosionDefaultForce = 500f;
+    [Header("Cubes Scatter Parameters")]
     [SerializeField] private float _pushForce = 10f;
     [SerializeField] private float _spinForce = 750;
 
@@ -11,18 +19,19 @@ public class CubeExploder : MonoBehaviour
     public void Explode(List<Cube> cubes, Cube originCube)
     {
         if(cubes != null)
-        {            
-            foreach (Cube cube in cubes)
-            {
-                Push(cube, originCube.transform.position);
-                Spin(cube);
-            }
+        {
+            ScatterSpawnedCubes(cubes, originCube);
+        }
+        else
+        {
+            PushCubesInRadius(originCube);
         }
 
-        Destroy(originCube);
+        PlayExplosionEffect(originCube.transform.position);
+        DestroyCube(originCube);
     }
     
-    private void Destroy(Cube cube)
+    private void DestroyCube(Cube cube)
     {
         cube.gameObject.SetActive(false);
         Destroy(cube.gameObject);
@@ -40,5 +49,46 @@ public class CubeExploder : MonoBehaviour
         Vector3 randomTorque = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
 
         cube.RigidBody.AddTorque(randomTorque * _spinForce);
+    }
+
+    private void PlayExplosionEffect(Vector3 position)
+    {
+        GameObject effect = Instantiate(_explosionEffect, position, Quaternion.identity);
+        effect.transform.localScale *= _explosionEffectScale;
+        Destroy(effect, _explosionEffectLifeTime);
+    }
+
+    private void ScatterSpawnedCubes(List<Cube> cubes, Cube originCube)
+    {
+        foreach (Cube cube in cubes)
+        {
+            Push(cube, originCube.transform.position);
+            Spin(cube);
+        }
+    }
+
+    private void PushCubesInRadius(Cube originCube)
+    {
+        Vector3 explosionEpicenter = originCube.transform.position;
+
+        float originCubeScale = originCube.transform.localScale.x;
+        float explosionRadius = _explosionDefaultRadius / originCubeScale;
+        float explosionForce = _explosionDefaultForce / originCubeScale;
+
+        Collider[] collisions = null;
+
+        collisions = Physics.OverlapSphere(explosionEpicenter, explosionRadius);
+
+        if(collisions.Length > 0)
+        {
+            foreach (Collider collider in collisions)
+            {
+                if(collider.TryGetComponent<Cube>(out Cube cube) & cube != originCube)
+                {
+                    Spin(cube);
+                    cube.RigidBody.AddExplosionForce(explosionForce, explosionEpicenter, explosionRadius);
+                }
+            }
+        }
     }
 }
